a242da443d65885c39ad7d3a4224de8347f6a60f
Juancorrea333 <juan.correa.zapata@correounivalle.edu.co>
2026-04-24 15:55:30 -0500

    Agrega estructura base del motor y abstracciones de carta

---
+++ src/MotorJuego.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MotorJuego {
+
+    private Jugador jugador1;
+    private Jugador jugador2;
+    private Jugador activo;
+    private Jugador oponente;
+    private int     numeroTurno;
+    private boolean juegoTerminado;
+    private Jugador ganador;
+
+    private boolean yaJugoUnaCarta;
+    private boolean yaAtaco;
+
+    private List<String> log = new ArrayList<>();
+
+    public MotorJuego(Jugador j1, Jugador j2) {
+        this.jugador1       = j1;
+        this.jugador2       = j2;
+        this.numeroTurno    = 1;
+        this.juegoTerminado = false;
+        this.ganador        = null;
+        this.yaJugoUnaCarta = false;
+        this.yaAtaco        = false;
+
+        if (Math.random() > 0.5) {
+            this.activo   = j1;
+            this.oponente = j2;
+        } else {
+            this.activo   = j2;
+            this.oponente = j1;
+        }
+    }
+}
