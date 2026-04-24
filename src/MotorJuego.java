486fe7024ea436c22ed4a8c422cbc8ecb7e83ccb
Juancorrea333 <juan.correa.zapata@correounivalle.edu.co>
2026-04-24 17:13:49 -0500

    Agrega metodo jugarCarta e inicio de ataque

--- src/MotorJuego.java
+++ src/MotorJuego.java
@@ -66,4 +66,93 @@ public class MotorJuego {
             log(activo.getNombre() + " roba una carta.");
         }
     }
+
+    public String jugarCarta(int indiceCarta, Posicion posicion, int indiceSacrificio) {
+        if (yaJugoUnaCarta) {
+            return "Ya jugaste una carta este turno.";
+        }
+
+        List<Carta> mano = activo.getMano();
+
+        if (indiceCarta < 0 || indiceCarta >= mano.size()) {
+            return "Carta no válida.";
+        }
+
+        Carta carta = mano.get(indiceCarta);
+
+        if (carta.esMonstruo() && carta.comoMonstruo().getNivel() > 4) {
+            if (!activo.tieneMonstruos()) {
+                return "Necesitas sacrificar un monstruo para invocar un monstruo de nivel 5 o más.";
+            }
+            List<Monstruo> campo = activo.getCampo();
+            if (indiceSacrificio < 0 || indiceSacrificio >= campo.size()) {
+                return "Índice de sacrificio no válido.";
+            }
+            activo.removerMonstruo(campo.get(indiceSacrificio));
+            log("¡Sacrificio realizado!");
+        }
+
+        if (carta.esMonstruo()) {
+            carta.comoMonstruo().setPosicion(posicion);
+        }
+
+        mano.remove(indiceCarta);
+        carta.jugar(activo, oponente);
+        log(activo.getNombre() + " juega: " + carta.getNombre());
+
+        if (carta.esMonstruo()) {
+            Monstruo m = carta.comoMonstruo();
+            if (m.getPosicion() == Posicion.ATAQUE) {
+                m.habilitarAtaque();
+            } else {
+                log(m.getNombre() + " está en DEFENSA y no puede atacar este turno.");
+            }
+            revisarTrampas(CondicionTrampa.AL_INVOCAR);
+        }
+
+        yaJugoUnaCarta = true;
+        verificarFinDeJuego();
+        return null;
+    }
+
+    public String atacar(int indiceAtacante, int indiceDefensor) {
+        if (esPrimerTurno()) {
+            return "No se puede atacar en el primer turno.";
+        }
+        if (yaAtaco) {
+            return "Ya realizaste un ataque este turno.";
+        }
+
+        List<Monstruo> campo = activo.getCampo();
+        if (indiceAtacante < 0 || indiceAtacante >= campo.size()) {
+            return "Monstruo atacante no válido.";
+        }
+
+        Monstruo atacante = campo.get(indiceAtacante);
+
+        if (!atacante.puedeAtacar()) {
+            return atacante.getNombre() + " no puede atacar (ya atacó o está en DEFENSA).";
+        }
+
+        revisarTrampas(CondicionTrampa.EN_ATAQUE);
+        if (juegoTerminado) {
+            yaAtaco = true;
+            return null;
+        }
+
+        if (!oponente.tieneMonstruos()) {
+            resolverAtaqueDirecto(atacante);
+        } else {
+            List<Monstruo> campoRival = oponente.getCampo();
+            if (indiceDefensor < 0 || indiceDefensor >= campoRival.size()) {
+                return "Monstruo defensor no válido.";
+            }
+            resolverCombate(atacante, campoRival.get(indiceDefensor));
+        }
+
+        atacante.agotarAtaque();
+        yaAtaco = true;
+        verificarFinDeJuego();
+        return null;
+    }
 }
